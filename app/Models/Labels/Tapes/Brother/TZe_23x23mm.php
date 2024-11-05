f571bd02eb73cdf3e4ee609b26bffb38735ca508
Mário <mario@epicfail.sk>
2024-11-05 17:50:28 +0100

    23x23 - v3

--- app/Models/Labels/Tapes/Brother/TZe_23x23mm.php
+++ app/Models/Labels/Tapes/Brother/TZe_23x23mm.php
@@ -8,7 +8,7 @@ use App\Models\Labels\Label;
 class TZe_23x23mm extends Label
 {
     private const BARCODE_MARGIN =   1.40;
-    private const TAG_SIZE       =   2.80;
+    private const TAG_SIZE       =   3.00;
     private const TITLE_SIZE     =   2.80;
     private const TITLE_MARGIN   =   0.50;
     private const LABEL_SIZE     =   2.00;
@@ -45,19 +45,20 @@ class TZe_23x23mm extends Label
         $currentY = $pa->y1;
         $usableWidth = $pa->w;
 
-        $barcodeSize = $pa->h;
+        $barcodeSize = $pa->h - self::TAG_SIZE;
 
         if ($record->has('barcode2d')) {
+            static::write2DBarcode(
+                $pdf, $record->get('barcode2d')->content, $record->get('barcode2d')->type,
+                $currentX, $currentY,
+                $barcodeSize, $barcodeSize
+            );
+            $barcodeSize += self::TAG_SIZE;
             static::writeText(
                 $pdf, "#" . $record->get('tag'),
                 $pa->x1, $pa->y2 - self::TAG_SIZE,
                 'freemono', 'b', self::TAG_SIZE, 'C',
                 $barcodeSize, self::TAG_SIZE, true, 0
-            );
-            static::write2DBarcode(
-                $pdf, $record->get('barcode2d')->content, $record->get('barcode2d')->type,
-                $currentX, $currentY,
-                $barcodeSize, $barcodeSize
             );
         } else {
             static::writeText(
